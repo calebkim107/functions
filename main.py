f3a994f97657a6d1e9c37a0f2706b51b5e50ed62
calebkim107 <calebkim1117@gmail.com>
2020-08-06 20:47:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+# FUNCTIONS - instructions to do a certain action
+
+# 2 things required for functions:
+# 1) define them
+# 2) call them to work
+
+#1) define function
+#
+
+
+
+
+
+
+
+# def sum(n1, n2):
+#   return n1 + n2
+
+# total = sum(2, 5)
+# print(total)
+
+# def sum(n1, n2):
+#   print(n1, n2)
+
+# sum(2, 5)
+
+
+
+
+def book():
+  print("Harry potter")
+
+book()
+
+
+def book():
+  print("Harry potter")
+
+book()
+
+def book2(title):
+  print(title)
+
+book2("Lord of rings")
+
+
